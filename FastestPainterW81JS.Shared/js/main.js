23de3c67d44fd5383e25488b1789d8a806e521f6
marcouberti <marcouberti84@gmail.com>
2014-09-20 15:11:22 +0200

    Fix "animationend" event never called.

--- FastestPainterW81JS.Shared/js/main.js
+++ FastestPainterW81JS.Shared/js/main.js
@@ -13,21 +13,22 @@ var init = function () {
     //ANIMAZIONI CSS3 su tutti gli elementi con classe "animatedButton"
     //Solo per gli elementi che dopo il click non reindirizzano ad una altra pagina
     //Altrimenti bisogna mettere il reindirizzamento dopo il termine dell'animazione.
-    $(".animatedButton").live("tap",function(event) {
-    	$(this).addClass('animating');
+    $(".animatedButton").live("tap", function (event) {
+        var btn = $(this);
+        btn.addClass('animating');
 	});
-    $(".animatedButton").live('webkitAnimationEnd', function(event){
+    $(".animatedButton").live('animationend', function (event) {
     	$(this).removeClass('animating');
 	});
-    $(".trickTrapImg").live('webkitAnimationEnd', function(event){
+    $(".trickTrapImg").live('animationend', function (event) {
     	//$(this).parent().removeClass('animatedTrickTrap');
     	$(this).parent().remove();
 	});
     //POPUP
-    $(".MS_popUpContainer").live('webkitAnimationEnd', function(event){
+    $(".MS_popUpContainer").live('animationend', function (event) {
     	$(this).removeClass('animatedPopUpCont');
 	});
-    $(".MS_popUpInn").live('webkitAnimationEnd', function(event){
+    $(".MS_popUpInn").live('animationend', function (event) {
     	$(this).removeClass('animatedPopUpInn');
 	});
     $(".JS_popUpClose").live("tap", function (event) {
